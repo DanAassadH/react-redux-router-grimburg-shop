c4d7e264ea86eea993ea64f4bd51ece474819208
Danish <danishriz100@gmail.comgit commitexit>
2020-09-26 23:57:07 -0600

    comparing shopping cart reducer

--- src/signIn/testing.js
+++ src/signIn/testing.js
@@ -83,3 +83,66 @@ const membersList = singleClan.members; */
 } 
 */
 
+
+export default () => {
+  const productsInCart1 = useSelector((state) => Object.values(state.cart));
+  const allProducts = useSelector((state) => Object.values(state.products));
+
+  const [productsInCart, setProductsInCart] = React.useState(productsInCart1);
+
+  let subTotal = 0;
+  let cartListArray=[];
+
+    for (const element of productsInCart)  {
+     const c = allProducts.find(xx => xx.id === element.productId);
+      c["quantity"] = element.quantity; 
+      subTotal = (c.price*c.quantity)+subTotal;
+      cartListArray.push(c);
+    }
+  
+  function DeleteProduct (id) {
+    const newList = cartListArray.filter((item) => item.id !== id);
+    setProductsInCart(newList);
+   }//end checkout fn
+
+  return (
+    <div id="checkout-page">
+
+    <h2>Your Cart</h2>
+
+      {cartListArray.map((cartItem)=>{
+          //  getchangeQuantity(cartItem.quantity)
+
+           return(
+
+          <ul className="products-listing">
+          <li id="image-p"><img src={`/imgs/products/${cartItem.image}`} alt="" /></li>
+          <li id="price-p">Unit Price: {cartItem.price}</li>
+          <li id="quantity-p">Quantity: </li>
+          <li id="remove-p">
+            <input type="textbox" value={cartItem.quantity}
+            ></input>
+          </li>
+          <li id="name-p">{cartItem.name}</li>
+          
+          <li id="remove-all">
+            <input type="button" value="Remove Item" onClick={() => DeleteProduct(cartItem.id)} ></input>
+          </li>
+        </ul>
+
+        );
+      })}
+     
+
+      <ul id="sub-total">
+        <li>Subtotal:</li>
+        <li>
+          <input type="text" value={`${subTotal} g`} ></input>
+        </li>
+      </ul>
+
+      <input type="submit" value="Checkout" onClick={CheckOut}></input>
+    </div>
+  );
+};
+
